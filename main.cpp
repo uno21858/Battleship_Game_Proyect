a45a7ab0c0037ade41195041ce9fc85950c23371
uno21858 <53879571+uno21858@users.noreply.github.com>
2024-11-21 21:47:05 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by elmer on 11/21/2024.
+//
+
+
+// logica del juego
+
+
+class Barco {
+
+
+  };
---
+++ tablero.cpp
@@ -0,0 +1,67 @@
+// Juego grafico de battleship grafico usando raylib
+
+// Librerias necesarias
+
+#include <raylib.h>
+
+
+// TAma;o del grid
+const int rows = 10;
+const int cols = 10;
+const int cellSize = 130;
+
+void DrawGameGrid(int offsetX, int offsetY) {
+    for (int row = 0; row < rows; row++) {
+        for (int col = 0; col < cols; col++) {
+            // Posición de la celda
+            int x = offsetX + col * cellSize;
+            int y = offsetY + row * cellSize;
+
+            // Dibujar los detalles de la celda
+            DrawRectangleLines(x - 2, y - 2, cellSize + 4, cellSize + 4, DARKGRAY); // Detalle más oscuro
+            DrawRectangleLines(x - 1, y - 1, cellSize + 2, cellSize + 2, GRAY);     // Detalle gris intermedio
+            DrawRectangleLines(x, y, cellSize, cellSize, LIGHTGRAY);                // Detalle principal
+        }
+    }
+}
+
+
+// Menu de inicio
+void DrawMenu() {
+    DrawText("Battleship", 190, 200, 20, LIGHTGRAY);
+
+    DrawText("1. Jugar", 190, 250, 20, LIGHTGRAY);
+
+    DrawText("2. Salir", 190, 300, 20, LIGHTGRAY);
+}
+
+
+int main()
+{
+    // Inicializar la ventana
+    const int screenWidth = 1300;
+    const int screenHeight = 1400;
+    InitWindow(screenWidth, screenHeight, "Battleship");
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+
+        // Draw
+        BeginDrawing();
+
+        ClearBackground(DARKBLUE);
+
+        //DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawGameGrid(0, 97);
+        EndDrawing();
+    }
+
+    // De-Initialization
+    CloseWindow();        // Close window and OpenGL context
+
+
+    return 0;
+}
+
